734a7db881b57f78f756d97573d128c942afbac3
Xenodll <103438156+Xeno-dll@users.noreply.github.com>
2024-08-07 22:42:57 +0300

    Update form-handler.js

--- form-handler.js
+++ form-handler.js
@@ -1,4 +1,4 @@
-document.getElementById('petition-form').addEventListener('submit', function(event) {
+document.getElementById('freeturkey').addEventListener('submit', function(event) {
     event.preventDefault();
 
     let formData = new FormData(this);
@@ -16,9 +16,9 @@ document.getElementById('petition-form').addEventListener('submit', function(eve
     .then(response => response.text())
     .then(result => {
         document.getElementById('message').innerText = 'Thank you for your support!';
-        document.getElementById('petition-form').reset(); // Optional: Reset the form after submission
+        document.getElementById('freeturkey').reset(); // Optional: Reset the form after submission
         // Update signature count
-        fetch('https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec')
+        fetch('https://script.google.com/macros/s/AKfycbyMEF2SRz2FV_4oDWK1s66b4T1Hq3Px379eu57SMenv2MaJPauMkbIBKNtE2cPJatJ9/exec')
             .then(response => response.text())
             .then(count => {
                 document.getElementById('signature-count').innerText = count;
